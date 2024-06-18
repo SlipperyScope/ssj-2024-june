6bedfe5a3193d7227f42b0d6027f1ba3d0cf8d87
Michael Lange <dingoeatingfuzz@gmail.com>
2024-06-18 01:00:44 -0700

    Example usage of motivations/engine

--- Game/Dev/Michael/Michael.gd
+++ Game/Dev/Michael/Michael.gd
@@ -17,6 +17,17 @@ func _ready():
 	for dist in AllDistricts.values():
 		dist.Econ.RawCost('Bed')
 
+	# Test Next and Motivations
+	var a = AllDistricts["Arts"]
+	a.Motivations.push_back(District.Motivate(District.MotivationType.Demand, "Bed", 1, 2, func(i): return i))
+
+	print("============== Motivations Test =============")
+	for i in range(5):
+		print("S", a.Supplies)
+		print("D", a.Demands)
+		print(len(a.Motivations), a.Motivations)
+		a.Next()
+
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
